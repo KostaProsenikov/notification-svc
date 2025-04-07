0374402701cb4c644ad660417d1cc6abdbf911c4
Prosenikov <k.prosenikov@blubito.com>
2025-04-07 18:13:51 +0300

    Add initial the notifications service

---
+++ src/main/java/app/model/NotificationPreference.java
@@ -0,0 +1,32 @@
+package app.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationPreference {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
+
+    private boolean isEnabled;
+
+    @Column(nullable = false)
+    private String contactInfo;
+
+}
